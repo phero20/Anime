a9dc7547d043ca93829b4ea34ab8db4a9a860ebe
Feroz <mdferozahmed27156@gmail.com>
2025-07-15 21:30:23 +0530

    Update Title.jsx

--- Frontend/src/components/Title.jsx
+++ Frontend/src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title({name, anime}) {
     return (
         <div className="mb-4">
-            <h2 className={`font-bold flex items-center tracking-tight gap-2 ${
+            <h2 className={`font-bold flex items-center tracking-tight gap-3 ${
                 anime 
                     ? 'text-xl md:text-xl lg:text-2xl xl:text-3xl' 
                     : 'justify-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl'
